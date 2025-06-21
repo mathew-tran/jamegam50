33182e56d342ec5232476be5bd9d5fab2969f7e7
mathew <mathew.tran.dev@gmail.com>
2025-06-21 08:08:28 +0800

    fix up scenes and use black screen instead of the placeholders.. for now

--- Scripts/InteractableItemChangeScenes.gd
+++ Scripts/InteractableItemChangeScenes.gd
@@ -2,6 +2,12 @@ extends InteractableItem
 class_name InteractableItemChangeScenes
 
 @export_file("*.tscn") var SceneToChangeTo
+@export var bHideIfCantBeInteracted = false
+
+func _ready() -> void:
+	if bHideIfCantBeInteracted:
+		if TriggerState == TRIGGER_STATE.CANNOT_BE_TRIGGERED:
+			visible = false
 
 func DoPostInteraction():
 	TransitionScene.TransitionToScene(load(SceneToChangeTo))
